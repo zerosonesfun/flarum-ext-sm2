2bc967078fd7380c2b0533e0aa77fe78c8d34cf0
Bungalow Bill <41351317+zerosonesfun@users.noreply.github.com>
2018-12-08 09:50:06 -0500

    Update extend.php

--- extend.php
+++ extend.php
@@ -17,6 +17,6 @@ use Flarum\Frontend\Document;
 return [
     (new Extend\Frontend('forum'))
         ->content(function (Document $document) {
-            $document->head[] = '<script src="/assets/extensions/zerosonesfun-sm2/soundmanager2.min.js"></script><script src="/assets/extensions/zerosonesfun-sm2/inlineplayer.js"></script><link rel="stylesheet" type="text/css" href="/assets/extensions/zerosonesfun-sm2/inlineplayer.css"><script src="/assets/extensions/zerosonesfun-sm2/forum.js"></script>';
+            $document->head[] = '<script src="/assets/extensions/zerosonesfun-sm2/soundmanager2.js"></script><script src="/assets/extensions/zerosonesfun-sm2/inlineplayer.js"></script><link rel="stylesheet" type="text/css" href="/assets/extensions/zerosonesfun-sm2/inlineplayer.css"><script src="/assets/extensions/zerosonesfun-sm2/reboot.js"></script>';
         })
 ];
