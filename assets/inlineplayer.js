ac84e2d58fd514401b039edfdd7a55d6d66f10da
Bungalow Bill <41351317+zerosonesfun@users.noreply.github.com>
2019-07-12 23:09:37 -0400

    Update inlineplayer.js

--- assets/inlineplayer.js
+++ assets/inlineplayer.js
@@ -245,10 +245,10 @@ window.inlinePlayer = inlinePlayer;
 
 soundManager.setup({
   // disable or enable debug output
-  debugMode: true,
+  debugMode: false,
   // use HTML5 audio for MP3/MP4, if available
   preferFlash: false,
-  useFlashBlock: true,
+  useFlashBlock: false,
   // path to directory containing SM2 SWF
   url: '../../swf/',
   // optional: enable MPEG-4/AAC support (requires flash 9)
